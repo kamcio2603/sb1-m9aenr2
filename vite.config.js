8cb5382580036f0f0997af8006a027b44f6ff2b8
kamcio2603 <kamcio2603@gmail.com>
2024-10-19 21:32:18 +0200

    another fix

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ export default defineConfig({
   build: {
     outDir: 'dist',
   },
+  rollupOptions: {
+    // If tweetnacl isn't getting bundled, try forcing Vite to include it:
+    external: ['tweetnacl'],
+  },
 })
